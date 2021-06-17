40bf347bad03a457844182d43428c3ab01334f5a
knoexus <knoexus@yahoo.com>
2021-06-17 09:51:36 +0300

    fix loading/error state for wallet connection

--- xstarter-app/src/components/ConnectButton.js
+++ xstarter-app/src/components/ConnectButton.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import StatusIcon from './StatusIcon'
+import Loading from './Loading'
 import Web3 from 'web3'
 
 import '../styles/main.css'
@@ -7,6 +8,7 @@ import '../styles/main.css'
 export default function ConnectButton({connectionCallback}) {
 
     const [isConnected, changeConnected] = useState(false)
+    const [connecting, changeConnecting] = useState(false)
     const [ethAddress, changeEthAddress] = useState("")
     const [connectError, changeConnectError] = useState(null)
 
@@ -14,11 +16,13 @@ export default function ConnectButton({connectionCallback}) {
         if (ethAddress !== "") connectionCallback(ethAddress)
     }, [ethAddress])
 
-    const errorNoWallet = <span className="error">Cannot connect to your wallet. Please make sure you have one <a target="_blank" rel="noopener noreferrer" href="https://metamask.io/">installed</a>  and your browser supports it.</span> 
-    const errorConnectionTrouble = <span className="error">Cannot connect to MetaMask. Please try again.</span>
+    const errorNoWallet = <span className="status-error">Cannot connect to your wallet. Please make sure you have one <a target="_blank" rel="noopener noreferrer" href="https://metamask.io/">installed</a>  and your browser supports it.</span> 
+    const errorConnectionTrouble = <span className="status-error">Cannot connect to MetaMask. Please try again.</span>
 
     const connectToWallet = async () => {
+        changeConnectError(null)
         if (window.ethereum) {
+            changeConnecting(true)
             window.web3 = new Web3(window.ethereum)
             try {
                 await window.ethereum.enable()
@@ -27,6 +31,7 @@ export default function ConnectButton({connectionCallback}) {
                 changeEthAddress(account)
             } catch (error) {
                 changeConnectError(errorConnectionTrouble)
+                changeConnecting(false)
             }
         } else {
             changeConnectError(errorNoWallet)
@@ -35,7 +40,7 @@ export default function ConnectButton({connectionCallback}) {
     
     return (
         <div className="div__connect-wallet">
-            <button disabled={isConnected} onClick={connectToWallet} type="button" className="button__outline button__connect-wallet">{isConnected ? <>Connected <StatusIcon/> </> : 'Connect Wallet' }</button> 
+            <button disabled={isConnected || connecting} onClick={connectToWallet} type="button" className="button__outline button__connect-wallet">{isConnected ? <>Connected <StatusIcon/> </> : connecting ? <Loading/> : 'Connect Wallet' }</button> 
             { connectError }
         </div>
     )
